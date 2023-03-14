febc483977f3111b019fc5aa2fcdff59b7a81ab6
stealmh <kmh922@naver.com>
2023-03-14 13:18:29 +0900

    Initial Commit

---
+++ AnimalCrossWiki/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimalCrossWiki
+//
+//  Created by 김민호 on 2023/03/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
