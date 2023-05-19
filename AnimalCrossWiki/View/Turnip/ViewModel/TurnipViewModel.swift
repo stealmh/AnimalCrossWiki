69dff47005b8d1a2641891b3dc136d0b0ce9e0bd
김민호 <66459715+stealmh@users.noreply.github.com>
2023-05-19 17:15:45 +0900

    [Add] TurnipViewModel 추가

---
+++ AnimalCrossWiki/View/Turnip/ViewModel/TurnipViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  TurnipViewMdoel.swift
+//  AnimalCrossWiki
+//
+//  Created by 김민호 on 2023/05/18.
+//
+
+import Foundation
+
+class TurnipViewModel {
+    func getTurnipResult(parameter: String) async throws -> Turnip {
+        let url = URL(string: "https://api.ac-turnip.com/data/?f=" + parameter)!
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        let (data, _) = try await URLSession.shared.data(for: request)
+        let result = try JSONDecoder().decode(Turnip.self, from: data)
+        print()
+        return result
+    }
+}
