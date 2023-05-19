cfe7819c9fb00d793b260e879a54f30740806426
김민호 <66459715+stealmh@users.noreply.github.com>
2023-05-19 14:53:25 +0900

    [Fix] UIBarButtonItem+util 기능 수정

--- AnimalCrossWiki/Extension/UIBarButtonItem+util.swift
+++ AnimalCrossWiki/Extension/UIBarButtonItem+util.swift
@@ -25,4 +25,22 @@ extension UIBarButtonItem {
 
         return menuBarItem
     }
+    
+    static func menuButton(name: String,
+                           color: UIColor,
+                           cornerRadius: CGFloat?,
+                           size:CGSize = CGSize(width: 100, height: 40)) -> UIBarButtonItem
+    {
+        let button = UIButton(type: .system)
+        button.setTitle(name, for: .normal)
+        button.backgroundColor = color
+        button.layer.cornerRadius = cornerRadius ?? 0.0
+
+        let menuBarItem = UIBarButtonItem(customView: button)
+        menuBarItem.customView?.translatesAutoresizingMaskIntoConstraints = false
+        menuBarItem.customView?.heightAnchor.constraint(equalToConstant: size.height).isActive = true
+        menuBarItem.customView?.widthAnchor.constraint(equalToConstant: size.width).isActive = true
+
+        return menuBarItem
+    }
 }
